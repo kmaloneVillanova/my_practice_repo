98cec94e7a0d82496831a8f72bf1d2a07eb96056
kmaloneVillanova <48385681+kmaloneVillanova@users.noreply.github.com>
2025-04-15 10:10:20 -0400

    added hw4

---
+++ hw4/sudokuAPI.js
@@ -0,0 +1,106 @@
+var sol =
+   [[0, 7, 0, 2, 3, 8, 0, 0, 0],
+    [0, 0, 0, 7, 4, 0, 8, 0, 9],
+    [0, 6, 8, 1, 0, 9, 0, 0, 2],
+    [0, 3, 5, 4, 0, 0, 0, 0, 8],
+    [6, 0, 7, 8, 0, 2, 5, 0, 1],
+    [8, 0, 0, 0, 0, 5, 7, 6, 0],
+    [2, 0, 0, 6, 0, 3, 1, 9, 0],
+    [7, 0, 9, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 9, 7, 4, 0, 8, 0]];
+var printBoard = function () {
+    for (var row = 0; row < 9; row++) {
+        for (var col = 0; col < 9; col++) {
+            var loc = "r" + (row + 1) + (col + 1);
+           // console.log("loc is " + loc);
+            var td = document.getElementById(loc);
+            td.innerHTML = "" + sol[row][col];
+        }
+    }
+};
+
+var loadNewPuzzle = function() {
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://sudoku-api.vercel.app/api/dosuku");
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+       // console.log("data is "+data.board);
+       console.log(data);
+        sol=data.newboard.grids[0].value;
+       // solveWithStack(sol);
+        printBoard(sol);
+    };
+    request.send();
+};
+
+var isInRow = function(row, number) {    
+    for (var col = 0; col < 9; col++) {
+        if (sol[row][col] == number) {
+          //  console.log("td is number");
+            return true;
+        }
+    }
+    return false;
+};
+
+var isInCol = function(col, number) {
+    
+    for (var row = 0; row < 9; row++) {
+        if (sol[row][col] == number) {
+        //    console.log("is in col "+td.innerHTML);
+            return true;
+        }
+    }     
+    return false;
+};
+
+// we check if a possible number is in its 3x3 box
+var isInBox = function(row, col, number) {
+    r = row - row % 3;
+    c = col - col % 3;
+
+    for (i = r; i < r + 3; i++) {
+        for (j = c; j < c + 3; j++) {
+            
+            if (sol[i][j] == number) {
+              //  console.log("is in box "+td.innerHTML);
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+var isOk = function(row, col, number) {
+    return !isInRow(row,number) && !isInCol(col,number) && !isInBox(row, col, number);
+}
+
+var solve = function () {
+    for (var row = 0; row < 9; row++) {
+        for (var col = 0; col < 9; col++) {
+         // we search an empty cell
+        
+         if (sol[row][col] == 0) {
+           // we try possible numbers
+           for (var number = 1; number <= 9; number++) {
+             if (isOk(row, col, number)) {
+               // number ok. it respects sudoku constraints
+               sol[row][col] = number;
+               if (solve()) { // we start backtracking recursively
+                 return true;
+               } else { // if not a solution, we empty the cell and we continue
+                 sol[row][col] = 0;
+               }
+            }
+        }
+        return false;
+    }
+}
+    }
+    printBoard();
+    return true;
+};
+//any code at bottom of file is exectued
+//think of this as the main method without the method declaration 
+printBoard();
